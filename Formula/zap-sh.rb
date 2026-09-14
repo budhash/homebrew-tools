72b5fa98a2b492f831d1787129214d91714c4fca
budhash <budhash@gmail.com>
2026-09-14 06:34:50 +0000

    zap-sh v1.1.3

--- Formula/zap-sh.rb
+++ Formula/zap-sh.rb
@@ -1,9 +1,9 @@
 class ZapSh < Formula
   desc "Lightning-fast bash script generator"
   homepage "https://github.com/budhash/zap-sh"
-  url "https://github.com/budhash/zap-sh/archive/refs/tags/v1.1.2.tar.gz"
-  sha256 "57becf69c9e73fe108b0aa30ce7084c2a12f29f1ae0adacc51163eb843408828"
-  version "1.1.2"
+  url "https://github.com/budhash/zap-sh/archive/refs/tags/v1.1.3.tar.gz"
+  sha256 "5e871bb3db38f6ebcd535dfe24c581f4526daab83c0da1d6f39c07dda9cb9636"
+  version "1.1.3"
   license "MIT"
 
   def install
